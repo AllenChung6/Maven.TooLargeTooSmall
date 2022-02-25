37a9c8e92f9e8a8dfdf85dd31c8ccc4f503c9230
Allen C <allenc@Zipcoders-MacBook-Pro.local>
2022-02-24 22:21:28 -0500

    Committing first round of Code. Initialied Random and called random class to generate number. Also created a checkNumber method.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,28 @@
-/**
- * Created by iyasuwatts on 10/17/17.
- */
-public class Main {
+import java.util.Random;
 
-    public static void main(String[] args){
-        
+
+public class Main {
+    public int guess;
+    public static int answer;
+
+
+    public boolean checkNumber(int guess) {
+        if (guess==answer){
+            return true;
+        } else {
+            return false;
+        }
     }
+
+    public static void main(String[] args) {
+        Random rand = new Random();
+        answer = rand.nextInt(50);
+
+
+
+        //System.out.println("Lets play a guessing game!");
+        //System.out.println("Try to guess a number from 0 - 50, please!");
+
+    }
+
 }
